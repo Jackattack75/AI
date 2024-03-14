ab30820fbc1c2d7b41ce5ae0e1cd765b743fe906
Sutton <jackevansutton@gmail.com>
2024-03-14 14:02:28 -0400

    PrintingNamesIndex - Stable - Pre ChatGPT

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -1,5 +1,11 @@
-# proggrammer: Mr Sutton
+# programmer: Mr Sutton
 # Date: 3/14/2024
 # program: Printing Names Index
 # resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
+names = ["Corey", "Chris", "Travis", "Dave"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
